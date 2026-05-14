ef195a8206e197ab70396f6fe9098cb38d345919
adptra01 <m.a.syaputra.94@gmail.com>
2026-05-14 16:38:51 +0700

    setup Breeze and Livewire

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Providers\AppServiceProvider;
+use App\Providers\VoltServiceProvider;
+
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
+    AppServiceProvider::class,
+    VoltServiceProvider::class,
 ];
