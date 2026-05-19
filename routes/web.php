898e8930c11551154b164a28ff932329acf16388
adptra01 <m.a.syaputra.94@gmail.com>
2026-05-19 23:26:44 +0700

    feat: implement database seeder, factory, GitHub service, and license observer to initialize system data

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::view('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+// Route::view('/', 'welcome', [
+//     'canRegister' => Features::enabled(Features::registration()),
+// ])->name('home');
 
-require __DIR__.'/settings.php';
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+})->name('home');
+
+require __DIR__ . '/settings.php';
